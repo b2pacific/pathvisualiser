d8ebfb6300bc6994c636f95a051122f82b3b804f
b2pacific <b2pacific@gmail.com>
2022-02-11 23:39:47 +0530

    Updated

--- src/howto.js
+++ src/howto.js
@@ -4,13 +4,13 @@ import { FaMapPin, FaMapMarkerAlt } from "react-icons/fa";
 const HowTo = () => {
   return (
     <div style={{}}>
-      <p style={{lineHeight: "40px"}}>
+      <div style={{ lineHeight: "40px" }}>
         <h3>Legend</h3>
         <FaMapMarkerAlt size="20px" color="blue" />
-        <span style={{marginLeft: "7px"}}>Source</span>
+        <span style={{ marginLeft: "7px" }}>Source</span>
         <br />
         <FaMapPin size="20px" color="red" />
-        <span style={{marginLeft: "7px"}}>Destination</span>
+        <span style={{ marginLeft: "7px" }}>Destination</span>
         <br />
         <div
           style={{
@@ -45,38 +45,33 @@ const HowTo = () => {
         ></div>
         Wall
         <br />
+      </div>
+      <br />
+      <h3>Steps for Path Finding</h3>
+      <ul>
+        <li>Place the Source and Destination Pointer</li>
+        <li>Select a Path Finding Algorithm from the Drop Down Menu</li>
+        <li>Click Find Path</li>
+      </ul>
+      <h3>Steps for Maze Generation and Path Finding</h3>
+      <ul>
+        <li>Select a Maze Generation Algorithm</li>
+        <li>Click Generate Maze</li>
+        <li>Place the Source and Destination Pointer</li>
+        <li>Select a Path Finding Algorithm from the Drop Down Menu</li>
+        <li>Click Find Path</li>
+      </ul>
+      <br />
+      <h3>Tips</h3>
+      <ul>
+        <li>
+          After a Path has been found and you change the position of the Source
+          or Destination Pointer then the Path Finding Algorithm will Re-run.
+        </li>
+      </ul>
+      <p style={{ marginTop: "10%", fontWeight: "bold" }}>
+        Made By Prashant Pandey
       </p>
-      <p>
-        <h3>Steps for Path Finding</h3>
-
-        <ul>
-          <li>Place the Source and Destination Pointer</li>
-          <li>Select a Path Finding Algorithm from the Drop Down Menu</li>
-          <li>Click Find Path</li>
-        </ul>
-        <h3>Steps for Maze Generation and Path Finding</h3>
-
-        <ul>
-          <li>Select a Maze Generation Algorithm</li>
-          <li>Click Generate Maze</li>
-          <li>Place the Source and Destination Pointer</li>
-          <li>Select a Path Finding Algorithm from the Drop Down Menu</li>
-          <li>Click Find Path</li>
-        </ul>
-      </p>
-      <p>
-        <h3>Tips</h3>
-
-        <ul>
-          <li>
-            After a Path has been found and you change the position of the
-            Source or Destination Pointer then the Path Finding Algorithm will
-            Re-run.
-          </li>
-        </ul>
-      </p>
-
-      <p style={{marginTop: "10%", fontWeight: "bold"}}>Made By <a href="https://prashantpandey.dev/" target="_blank">Prashant Pandey</a></p>
     </div>
   );
 };
